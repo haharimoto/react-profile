3527c82cc83b54f68dd868e0a9ead52bfd180eb2
Hakuyo Harimoto <haharimoto@gmail.com>
2023-03-24 23:04:10 +0900

    deleted unnecessary files

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      Header
+      {/* <header>
+        <a href="https://github.com/">
+          <i className="fa-brands fa-github" style="font-size: 1.8em;"></i>
+        </a>
+      </header> */}
+    </div>
+  )
+}
+
+export default Header
