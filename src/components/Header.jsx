a45ef5a07b11f056f4deb09b9ec21d64c5ac086e
Hakuyo Harimoto <haharimoto@gmail.com>
2023-06-08 11:01:08 +0900

    styling navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 function Header() {
   return (
-    <div>
-      <header>
-        <a href="https://github.com/">
-          <FontAwesomeIcon icon={faGithub} style={{height: "32px", width: "32px", color: "white"}} />
-        </a>
-      </header>
-    </div>
+    <header>
+      <img className="icon" src="https://images.unsplash.com/photo-1569982175971-d92b01cf8694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80" alt="" />
+      <div className="links">
+        <a href='/'>About</a>
+        <a href='/'>Portfolio</a>
+        <a href='/'>Contents</a>
+        <a href='/'>Contact</a>
+      </div>
+    </header>
   )
 }
 
