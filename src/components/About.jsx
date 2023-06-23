8669c603fdb35b78a74d94bfdcbbf3986c1f2aa8
Hakuyo Harimoto <haharimoto@gmail.com>
2023-06-23 15:55:00 +0900

    styled about section as flexbox

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function About() {
+
   return (
-    <div id='about'>About</div>
+    <div id='about'>
+      <div className="hero">
+        <div className="hero--text">
+          <h1>From Concepts to Creations</h1>
+          <p>Bridging the Gap Between Imagination and Reality</p>
+        </div>
+        <div className="hero--animation">Sakura</div>
+      </div>
+    </div>
   )
 }
 
