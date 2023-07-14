d6521025174d2e4efd394a7b339dc1bb59810fb1
Hakuyo Harimoto <haharimoto@gmail.com>
2023-07-14 22:52:48 +0900

    implementing carousel for my projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+// import macbook from '../../public/macbookFrame.png'
+// import iphone from '../../public/iphone14Max.png'
+import imageApp from '../../public/carousel/images/imageApp.png'
+
+
 
 function Project() {
+  const slides = [
+    {image: '../../public/carousel/images/imageApp.png', name: 'imageApp'},
+    {image: '../../public/carousel/images/memeGenerator.png', name: 'memeGenerator'},
+    {image: '../../public/carousel/images/photoRonin.png', name: 'photoRonin'}
+  ]
+
   return (
     <div id='project'>
       <div className="project-container">
-        <ul>
-          <li>Project 1</li>
-          <li>Project 2</li>
-          <li>Project 3</li>
-          <li>Project 4</li>
-        </ul>
+        <img src={imageApp} alt="" />
+
+        {/* {slides.map((slide, index) => (
+          <li key={index}>
+            <img src={slide} alt="" />
+          </li>
+        ))} */}
       </div>
     </div>
   )
 }
 
 export default Project
+
+
+{/* <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8" alt="" /> */}
