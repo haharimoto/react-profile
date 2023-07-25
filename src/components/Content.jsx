cb37cb91b7597bce7bfa4adb4d5fb680adac6d03
Hakuyo Harimoto <haharimoto@gmail.com>
2023-07-25 14:17:59 +0900

    implemented grid in the content section and tweaked some styles

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Content() {
   return (
-    <div id='content'>Content</div>
+    <div id='content'>
+      <div className="grid-container">
+        <div className="grid-container--portrait"></div>
+        <div className="grid-container--square first"></div>
+        <div className="grid-container--landscape first"></div>
+        <div className="grid-container--landscape second"></div>
+        <div className="grid-container--square second"></div>
+      </div>
+    </div>
   )
 }
 
