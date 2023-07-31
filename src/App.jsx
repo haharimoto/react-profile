4a2b191a481312d0255cd5710cb95da3468d847e
Hakuyo Harimoto <haharimoto@gmail.com>
2023-07-31 17:10:27 +0900

    added a tiny touch of purple tint to the linear gradient of the image overlay in the Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import About from './components/About'
 import Project from './components/Project'
 import Content from './components/Content'
-import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 
