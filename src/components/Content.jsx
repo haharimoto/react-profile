0d41e4c0e1b8cf42b7d3262f468bda83e264f5e1
Hakuyo Harimoto <haharimoto@gmail.com>
2023-08-09 12:49:33 +0900

    finished implementing grid and its transition animation using react-spring

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
+import { animated, useSpring } from 'react-spring';
 
 function Content() {
   const [isShowMore, setIsShowMore] = useState(false)
 
-  const gridStyles = isShowMore ? {
-    gridTemplateRows: 'repeat(12, 120px)'
-  } : {}
+  // React-Spring
+  const animation = useSpring({
+    // maxHeight: isShowMore ? '1440px' : '480px',
+    // after adding gap =>
+    maxHeight: isShowMore ? '1580px' : '540px',
+    config: { tension: 120, friction: 14 }
+  });
 
   return (
     <div id='content'>
-      <div className="grid-container" style={gridStyles}>
+      <animated.div className="grid-container" style={animation}>
 
         <div className="grid-container--portrait first">
           <div className='grid-container--portrait first--text'>
@@ -47,10 +52,15 @@ function Content() {
           </>
          )
         }
-      </div>
+      </animated.div>
       <button onClick={() => {setIsShowMore(!isShowMore)}}>button</button>
     </div>
   )
 }
 
 export default Content
+
+
+// Instead of using CSS for the grid expansion and collapse, use React Spring, a physics-based animation library. This library provides a more intuitive way to handle complex transitions.
+
+// By using react-spring, you're allowing the library to handle the physics-based animation. This approach should provide a smoother transition for both expansion and collapse. Remember, this still doesn't "transition" the grid-template-rows property, but it does achieve a similar effect by transitioning the maxHeight of the container with a smooth animation.
