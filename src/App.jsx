dee3359a375ab2c6b8339d4c3d87aa77bc6f077e
Hakuyo Harimoto <haharimoto@gmail.com>
2023-09-04 04:55:43 +0900

    reverted back

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import ProjectPage from './components/ProjectPage'
-import ErrorPage from './components/ErrorPage'
 import './App.css'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import About from './components/About'
+import Project from './components/Project'
+import Content from './components/Content'
+import Contact from './components/Contact'
+import ProjectPage from './components/ProjectPage'
+import Preloader from './components/Preloader'
+import ErrorPage from './components/ErrorPage'
 
 
 function App() {
+  const [showLoad, setShowLoad] = useState(false)
+
+  //* works now but need some improvements, especially for slower networks
+  // settimeout used for optimizing preloader animation since About component is task heavy and causing preloader animation to lag
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoad(true)
+    }, 3300)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+
   return (
     <BrowserRouter>
       <div className="App">
+        {!showLoad && <Preloader />}
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={
+            <>
+              <Header />
+              <Hero />
+              <About />
+              <Project />
+              <Content />
+              <Contact />
+            </>
+          } />
           <Route path='/project' element={<ProjectPage />} />
           <Route path='*' element={<ErrorPage />} />
         </Routes>
